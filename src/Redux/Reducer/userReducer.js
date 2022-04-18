2ac02d8cadadcc8785a0fcac1efcbf15e51477ef
Thao Ha <thao.ha@employmenthero.com>
2022-04-18 13:06:09 +0700

    add redux

---
+++ src/Redux/Reducer/userReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  credentials: null,
+  isAuthenticated: false,
+};
+
+const UserReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default UserReducer;
